ff239abccb681cb7e50804a0796a52f85757c82e
Alexis Ducoulombier <alexisducoulombier@hotmail.fr>
2020-04-14 14:50:55 +0200

    First/Last Name

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,4 +1,19 @@
 <?php
+
 class Form{
-    
+
+    public $action;
+    public $method;
+    public $fields;
+
+    function __construct($action,$method){
+        $this->action = $action;
+        $this->method = $method;
+    }
+
+    function addTextField(string $fieldName, string $fieldValue){
+        $this->fields[$fieldName] = $fieldValue;
+        return $this;
+    }
+
 }
--- 1-generation-formulaire/index.php
+++ 1-generation-formulaire/index.php
@@ -3,11 +3,21 @@ include 'Form.php';
 
 // Des variables par défaut pour vos tests.
 
+
+$lastname = 'GERARD';
+$firstname = 'CREPIN';
+$method = '_POST';
+$action = '#';
+
 // YOUR CODE HERE
 
 $form = new Form($action, $method);  // créer le début du formulaire
-$form->addTextField('nom',$nom); // créer un input de type texte avec comme valeur par défaut $nom
-$form->addTextField('prenom',$prenom); // créer un input de type texte avec comme valeur par défaut $prenom
-$form->addSubmitButton('Modifier'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
+$form->addTextField('lastname',$lastname); // créer un input de type texte avec comme valeur par défaut $nom
+$form->addTextField('firstname',$firstname); // créer un input de type texte avec comme valeur par défaut $prenom
+//$form->addSubmitButton('submit'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
 
-echo $form->build(); // générer le formulaire
+foreach ($form->fields as $field) {
+    echo $field.' ';
+}
+
+//echo $form->build(); // générer le formulaire
