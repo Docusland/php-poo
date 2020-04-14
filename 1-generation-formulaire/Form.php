6dc1651c0f60234838961d3caab47cb6205cf04c
Alexis Ducoulombier <alexisducoulombier@hotmail.fr>
2020-04-14 15:52:48 +0200

    build

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -5,6 +5,8 @@ class Form{
     public $action;
     public $method;
     public $fields;
+    public $fieldName;
+    public $fieldValue;
 
     function __construct($action,$method){
         $this->action = $action;
@@ -14,6 +16,18 @@ class Form{
     function addTextField(string $fieldName, string $fieldValue){
         $this->fields[$fieldName] = $fieldValue;
         return $this;
+    }
+
+    function addSubmitButton(string $buttonName){
+    }
+
+    function build(){
+       return 
+       '<form action="'.$this->action .'" method="'. $this->method.'">
+        <input name="lastname" value="'.$this->fieldName.'" >
+        <input name="firstname" value="'.$this->fieldValue.'" >
+        <input type="submit" value="Save" >
+        </form>';
     }
 
 }
--- 1-generation-formulaire/index.php
+++ 1-generation-formulaire/index.php
@@ -14,10 +14,10 @@ $action = '#';
 $form = new Form($action, $method);  // créer le début du formulaire
 $form->addTextField('lastname',$lastname); // créer un input de type texte avec comme valeur par défaut $nom
 $form->addTextField('firstname',$firstname); // créer un input de type texte avec comme valeur par défaut $prenom
-//$form->addSubmitButton('submit'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
+$form->addSubmitButton('Save'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
 
 foreach ($form->fields as $field) {
     echo $field.' ';
 }
 
-//echo $form->build(); // générer le formulaire
+echo $form->build(); // générer le formulaire
