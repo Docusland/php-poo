90c104c75ac3505a2b999b190f72774f9537d0d7
Chauvac Henri <chenri@linux.home>
2020-04-14 16:21:48 +0200

    code fucked up

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,61 +1,118 @@
 <?php
-class Form{
-    private $action;
-    private $method;
-    private $html;
 
-    public function __construct(string $action, string $method) {
-        $this->action = $action;
-        $this->method = $method;
-        $this->html = $this->addForm();
-    }
+abstract class HtmlField {
+    private $name;
+    private $value;
 
-    private function addForm() {
-        return "<form action=\"".$this->action." method=\"".$this->method."\">";
+    private function isValid() {
+        //a faire
+        return true;
     }
-
-    public function addTextField(string $name, string $value) {
-        $this->html .= "<input type=\"text\" name=\"".$name."\" value=\"".$value."\">";
-        return $this;
-    }
-    public function addTextArea(string $name, string $value){
-        $this->html .= "<input type=\"textarea\" name=\"".$name."\" value=\"".$value."\">";
-        return $this;
-    }
-    public function addSelectField(array $options) {
-        $this->html .= "<select>";
-        foreach ($options as  $option) {
-            $this->html .= '<option>'.$option.'</option>';
+    public function __construct($name, $value) {
+        if (!$this->isValid()) {
+            throw new Exception('Please set a value');
         }
-        $this->html .= '</select>';
-        return $this;
-    }
-    public function addRadioButton(string $name, string $value) {
-        $this->html .= "<input type=\"radio\" name=\"".$name."\" value=\"".$value."\">";
-    }
-    public function addSubmitButton($value) {
-        $this->html .= "<input type=\"submit\" value=\"".$value."\">";
-        return $this;
-    }
-    public function  build() {
-        return $this->html."</form>";
+        $this->name = $name;
+        $this->value = $value;
     }
 }
 
-// class Validator{
-//     private $errMessage;
+class TextField extends HtmlField {
+    private function isValis() {
+        //a faire
+        return true;
+    }
+    public function __toString() {
+        return "<input type=\"text\" name=\"".$this->name."\" value=\"".$this->value."\">";
+    }
+}
 
-//     public function __construc(string $errMessage) {
-//         $this->errMessage = $errMessage;
-//     }
-//     public function isValid($fields) {
-//         $valid = true;
+class TextAreaField extends HtmlField {
+    private function isValis() {
+        //a faire
+        return true;
+    }
+    public function __toString() {
+        return "<input type=\"textarea\" name=\"".$name."\" value=\"".$value."\">";
+    }
+}
 
-//         foreach ($fields as field) {
-//             if (!$field->isValid()) {
-//                 $valid = false;
-//             }
-//         }
-//         return $valid;
-//     }
-// }
+class RadioField extends HtmlField {
+    private function isValis() {
+        //a faire
+        return true;
+    }
+    public function __toString() {
+        return "<input type=\"radio\" name=\"".$name."\" value=\"".$value."\">";
+    }
+}
+
+class Form{
+    private $fields;
+
+    public function __construct(string $action, string $method) {
+        $this->fields = $this->addForm($action, $method);
+    }
+
+    private function addForm(string $action, string $method) {
+        return "<form action=\"".$action." method=\"".$method."\">";
+    }
+
+    public function addTextField(string $name, string $value) {
+        $this->fields[] = new TextField($name, $value);
+        return $this;
+    }
+    public function addTextArea(string $name, string $value){
+        $this->fields[] = new TextAreaField($name, $value);
+        return $this;
+    }
+    public function addSelectField(string $name, array $options) {
+        $select .= "<select name=\"$name\">";
+        foreach ($options as  $option) {
+            $select .= '<option>'.$option.'</option>';
+        }
+        $select .= '</select>';
+        $this->fields[] = $select;
+        return $this;
+    }
+    public function addRadioButton(string $name, string $value) {
+        $this->fields[] = new RadioField($name, $value);
+        return $this;
+    }
+    public function addSubmitButton($value) {
+        $this->fields[] = "<input type=\"submit\" value=\"".$value."\">";
+        return $this;
+    }
+    public function  build() {
+        foreach ($this->fields as $field) {
+            $html .= $field;
+        }
+        return $html."</form>";
+    }
+}
+
+class Validator{
+    private $errMessage;
+
+    public function __construc(string $errMessage) {
+        $this->errMessage = $errMessage;
+    }
+    public function isValid($fields) {
+        $valid = true;
+
+        foreach ($fields as $field) {
+            if (!$field->isValid()) {
+                $valid = false;
+            }
+        }
+        return $valid;
+    }
+    public function setErrMessage(string $errMessage) {
+        if (is_string($errMessage)) {
+            $this->errMessage = $errMessage;
+        }
+    }
+    public function getErrMessage() {
+        return $this->errMessage;
+    }
+}
