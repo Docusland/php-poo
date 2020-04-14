7eca703bdf981736e49d7494c31e97702bcbf0ba
grollote <grolleau.annelise@gmail.com>
2020-04-14 15:57:48 +0200

    premier rendu

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,4 +1,37 @@
 <?php
-class Form{
+
+    class Form{
+        public $action;
+        public $method;
+        public $fieldname;
+        public $fieldvalue;
+        public $button;
+
+     
     
-}
+        public function __construct(Action $action, Method $method){
+            $this->action = $action;
+            $this->method = $method;
+        }
+
+        public function addTextField(string $fieldname, string $fieldvalue){
+            $this->fields[$fieldname]=$fieldvalue; 
+        }
+
+        public function addSubmitButton(){
+            $this->button='<input type='text''name'=".$field."
+
+        }
+        
+        public function build (){
+
+        }
+    }
+
+    class Field{
+        private $textfield;
+        private $numberfield;
+    }
+    
+   
+?>
