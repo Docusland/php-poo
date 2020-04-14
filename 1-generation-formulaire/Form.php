13c878f4085f27024b705e981766e69b5da51a54
kaavodd <killianhers@gmail.com>
2020-04-14 15:45:26 +0200

    Avancement du code

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -2,36 +2,23 @@
 class Form{
     private $action;
     private $method;
-    private $lastname =[];
-    private $firstname =[];
+    private $fieldName;
+    private $fieldValue;
 
-    function addTextField(string $lastnames ,string $firstnames){
-        $this->lastname[] =$lastnames;
-        $this->firstname[] =$firstnames;
+    function __construct($action,$method){
+        $this->action = $action;
+        $this->method = $method;
+    }
+
+    function addTextField(string $fieldName ,string $fieldValue){
+        $this->fields[$fieldName] = $fieldValue;
         return $this;
     }
     function addSubmitButton(string $lastnames ,string $firstnames){
-        $this->lastname[] =$lastnames;
-        $this->firstname[] =$firstnames;
-        return $this;
+        
     }
-    public function getlastname(){
-        return $this->lastname;
+    public function build(){
+        echo $this->action;
+        echo $this->method;
     }
-    public function setlastname (string $lastnames){
-        $this->lastname = $lastname;
-    }
-    public function getfirstname(){
-        return $this->firstname;
-    }
-    public function setfirstname (string $firstnames){
-        $this->firstname = $firstname;
-    }
-    public function getaction(){
-        return $this->action;
-    }
-    public function setaction (string $action){
-        $this->action = $action;
-    }
-
 }
