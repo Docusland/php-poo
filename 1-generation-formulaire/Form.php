7cf0d8709788194a93475122165602716039e251
harfang <raupasto@gmail.com>
2020-04-14 16:05:20 +0200

    exercice du jour

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,4 +1,28 @@
-<?php
+.<?php
 class Form{
+private $action;
+private $method;
+private $fieldName;
+private $fieldValue; 
+
+function __construct($action,$method){ //plus rapide
+    $this->action = $action;
+    $this->method = $method;
+}
+
+
+//créer un input avec $nom par défaut
+
+function addTextField(string $fieldName ,string $fieldValue){
+    $this->fields[$fieldName] = $fieldValue;
+    return $this;
+}
+/*
+function addSubmitButton(string $lastnames ,string $firstnames){
     
+}
+public function build(){
+    echo $this->action;
+    echo $this->method;
+}*/
 }
