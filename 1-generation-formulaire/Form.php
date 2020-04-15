75eedb3e7e459478ce1d213693821631ce88ae63
Grollote <58036756+Grollote@users.noreply.github.com>
2020-04-15 16:38:20 +0200

    Add files via upload

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,37 +1,57 @@
 <?php
+class Form{
+    private $fields=[];
+    private $method;
+    private $action;
+    private $button;
 
-    class Form{
-        public $action;
-        public $method;
-        public $fieldname;
-        public $fieldvalue;
-        public $button;
-
-     
-    
-        public function __construct(Action $action, Method $method){
-            $this->action = $action;
-            $this->method = $method;
-        }
-
-        public function addTextField(string $fieldname, string $fieldvalue){
-            $this->fields[$fieldname]=$fieldvalue; 
-        }
-
-        public function addSubmitButton(){
-            $this->button='<input type='text''name'=".$field."
-
-        }
-        
-        public function build (){
-
-        }
+ /*Create a magic thing*/
+    public function __construct (String $action, String $method){
+        $this->action = $action;
+        $this->method = $method;
+    }
+/*Create a new field*/
+    public function addTextField(String $fieldName, String $fieldValue){
+        $this->fields[] = "<input type='text' name='$fieldName' value='$fieldValue'>";
+        return $this;
+    }
+    public function addNumberField(String $fieldName, int $fieldValue){
+        $this->fields[] = "<input type ='number' name='$fieldName' value='$fieldValue'>";
+        return $this;
+    }
+/*Create a Checkbox*/
+    public function addCheckboxField (String $fieldName, bool $fieldValue){
+        $checked=($fieldValue)?'checked':'';
+        $this->fields[]="<input type='checkbox' name='$fieldName' $checked>";
+        return $this;
     }
 
-    class Field{
-        private $textfield;
-        private $numberfield;
+/*Create a list of options*/
+    public function addSelectField(array $options, String $fieldName, int $fieldValue){
+        $html ="<select name=$fieldName>";
+        foreach ($options as $i=>$option){
+            $html .= "<option value ='$i'>$option</option>";
+        }
+        $html .="</select>";
+        $this->fields[] = $html;
+        return $this;
+    }
+
+/* Create'Submit' button*/
+    public function addSubmitButton($text){
+        $this->button = "<input type='submit' value='$text'>";
+    }
+
+/*Form's recording*/
+    public function build(){
+        $html="<form action='$this->action' method='$this->method'>";
+        foreach($this->fields as $field){
+            $html .=$field;
+        }
+        $html .=$this->button;
+        $html .='</form>';
+        return $html;
+
     }
     
-   
-?>
+}
