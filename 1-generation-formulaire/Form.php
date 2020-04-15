f3868e4fde97e9f3aa94eeddee0bdb1252037237
kaavodd <killianhers@gmail.com>
2020-04-15 16:14:48 +0200

    Etape 2 Killian Hersant

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -2,23 +2,51 @@
 class Form{
     private $action;
     private $method;
-    private $fieldName;
-    private $fieldValue;
+    private $fields;
+    private $button;
 
-    function __construct($action,$method){
+    function __construct(String $action,String $method){
         $this->action = $action;
         $this->method = $method;
     }
 
     function addTextField(string $fieldName ,string $fieldValue){
-        $this->fields[$fieldName] = $fieldValue;
+        $this->fields[] = "<input type = 'text' name = '$fieldName' value = '$fieldValue'>";
         return $this;
     }
-    function addSubmitButton(string $lastnames ,string $firstnames){
-        
+    function addSubmitButton(String $text){
+        $this->button= "<input type = 'submit' value ='$text'>";        
     }
+    public function addNumberField(String $fieldName, int $fieldValue) {
+        $this->fields[] = "<input type='number' name='$fieldName' value='$fieldValue'>";
+        return $this;
+    }
+    function addCheckboxField(string $fieldName ,string $fieldValue){
+        $checked = ($fieldValue)?'checked':'';
+        $this->fields[] = "<input type = 'checkbox' name = '$fieldName' $checked>";
+        return $this;
+    }
+    function addSelectField( $options,string  $name , $method){
+
+        $html= "<select name='$name'>";
+        foreach ($options as $key => $option){ 
+            $html .= "<option value='$key'>
+            $option
+            </option>";
+        }
+        $this->fields[] = $html;
+        $html .= "</select>";
+        return $this;
+    }
+    
     public function build(){
-        echo $this->action;
-        echo $this->method;
+        
+        $html = "<form action='$this->action' method='$this->method'>";
+        foreach($this->fields as $field){
+            $html .= $field;
+        }
+        $html .= $this->button;
+        $html .= '</form>';
+        return $html;
     }
 }
--- 1-generation-formulaire/index.php
+++ 1-generation-formulaire/index.php
@@ -4,10 +4,24 @@ include 'Form.php';
 // Des variables par défaut pour vos tests.
 
 // YOUR CODE HERE
+$action = '#';
+$method = 'POST';
+$name = 'Pandémie';
+$min_age = 14;
+$min_valeurs = 0;
+$max_valeurs = 4;
+$is_available = (bool) true;
+$options = ['Bernard ','jean-claude','yves' , 'Antoine' , 'Didier' , 'Anne-Lise'];
 
 $form = new Form($action, $method);  // créer le début du formulaire
-$form->addTextField('nom',$lastname); // créer un input de type texte avec comme valeur par défaut $nom
-$form->addTextField('prenom',$firstname); // créer un input de type texte avec comme valeur par défaut $prenom
-$form->addSubmitButton('Modifier'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
+$form->addTextField('name',$name); // créer un input de type texte avec comme valeur par défaut $nom
+$form->addTextField('prenom',$name); // créer un input de type texte avec comme valeur par défaut $prenom
+$form->addNumberField('min_age',$min_age);
+$form->addNumberField('min_valeurs',$min_valeurs);
+$form->addNumberField('max_valeurs',$max_valeurs);
+$form->addCheckboxField('is_available', $is_available);
+$form->addSubmitButton('Save'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
+$form->addSelectField($options, $name , $method);
+
 
 echo $form->build(); // générer le formulaire
