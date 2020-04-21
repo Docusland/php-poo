e5321200e22bcab5449a7558f69cf658a431265b
Marin <marinplt@gmail.com>
2020-04-21 15:44:08 +0200

    21/04 3eme exo

---
+++ 3-php-training-mysql/read.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Jeux de société</title>
+  </head>
+  <body>
+    <h1>Liste des jeux de société</h1>
+    <!-- Afficher la liste des jeux -->
+    <?php echo $reponse; ?>
+  </body>
+</html>
+<?php 
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=boardgames;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT * from name');
+?>
