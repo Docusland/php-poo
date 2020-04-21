1c8ed8a5d666ae3f3be7d532f8ccaf62215ea738
Sagory <quentin0853@msn.com>
2020-04-21 11:25:25 +0200

    Exercice 2 fini(oubli commit)

--- 2-refactoring-de-classe/index.php
+++ 2-refactoring-de-classe/index.php
@@ -1,5 +1,24 @@
 <?php
-include 'Form_2.php';
+    include 'classes/Form_2.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP POO</title>
+    <style>
+        .error{
+            color:red;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+
+
+<?php
 
 // YOUR CODE HERE
 $action = '#';
@@ -11,12 +30,18 @@ $max_players = 4;
 $is_available = (bool) true;
 
 $form = new Form($action, $method);
+try {
 $form->addTextField('name',$name); 
 $form->addNumberField('min_age',$min_age);
 $form->addNumberField('min_players',$min_players);
 $form->addNumberField('max_players',$max_players);
 $form->addCheckboxField('is_available', $is_available);
-
 $form->addSubmitButton('Modifier');
+} catch (Exception\FormException $e) {
+    echo "<span class='error'>", $e->getMessage(), "</span>";
+}
 
 echo $form->build();
+
+
+?>
